49ee80bdf377e158da7996278b007b87ad66e49d
Rômulo Guimarães <romulo1984@gmail.com>
2021-07-19 20:54:40 -0300

    Use axios

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
 module.exports = (req, res) => {
     const API_URL = 'https://geql.globo.com/graphql'
-    fetch(API_URL, {
-            mode: 'no-cors',
-            method: 'post'
-        })
-        .then(blob => blob.json())
-        .then(json => [
-            res.status(200).send(json)
-        ])
+
+    axios(API_URL, {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+        },
+        withCredentials: true,
+        credentials: 'same-origin'
+    }).then(response => {
+        res.status(200).send(response)
+    })
 }
