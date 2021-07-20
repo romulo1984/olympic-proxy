1668a80abbfee34890d1fc00222f4e3df421bdcd
Rômulo Guimarães <romulo1984@gmail.com>
2021-07-19 22:14:28 -0300

    Pass query by body

--- api/index.js
+++ api/index.js
@@ -2,72 +2,33 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 const cors = require('cors')
-const bodyParser = require('body-parser')
 
 app.listen(80)
-
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-const query = `
-    query GET_MEDALS {
-        medalTable {
-            ranking
-            country {
-              code
-              name
-              flag {
-                svg {
-                  name
-                  url
-                  width
-                  height
-                  type
-                  rightsHolder
-                  isAnimated
-                }
-              }
-            }
-            gold
-            silver
-            bronze
-            total
-          }
-          sports {
-            code
-            name
-            hasMedal
-            imageURL(format: SVG)
-          }
-    }
-`
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.post('/api', async(req, res) => {
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
     const API_URL = 'https://geql.globo.com/graphql'
 
-    console.log('api url', API_URL)
-
-    const result = axios.post(API_URL, {
+    const result = await axios.post(API_URL, {
         headers: {
             'Access-Control-Allow-Origin': '*',
             'Content-Type': 'application/json'
         },
-        query
+        query: req.body.query
     }).then(response => {
-        console.log(response.data)
+        console.log('data', response.data)
         console.log(response.headers)
         console.log(response.status)
         return response
     }).catch(error => {
+        console.log({ error })
         return error
     })
 
-    console.log('result', JSON.stringify(result))
-
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.write(JSON.stringify(result))
-    res.end()
+    res.send(result.data)
 })
 
 app.get('/api/test', (req, res) => {
